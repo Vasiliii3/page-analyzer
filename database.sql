1d32a1158dda7e2468b1529bacc0abba01b34117
Vasilii <petgga@gmail.com>
2023-01-29 16:17:12 +0300

    add url_checks

--- database.sql
+++ database.sql
@@ -1,7 +1,18 @@
 drop table if exists urls CASCADE ;
+drop table if exists url_checks CASCADE ;
 
 create table urls(
 id smallserial primary key,
 name varchar(255) unique,
+created_at DATE
+);
+
+create table url_checks(
+id smallserial primary key,
+url_id INTEGER REFERENCES urls (Id) on delete cascade on update cascade,
+status_code INTEGER,
+h1 varchar(255),
+title varchar(255),
+description text,
 created_at DATE
 );
