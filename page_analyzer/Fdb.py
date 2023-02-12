4be62f126faeab712e2c0a5ecaeda11839ee4df9
Vasilii <petgga@gmail.com>
2023-02-12 18:14:32 +0300

    add class db

---
+++ page_analyzer/Fdb.py
@@ -0,0 +1,100 @@
+from psycopg2.extras import RealDictCursor
+from psycopg2.errorcodes import UNIQUE_VIOLATION
+import psycopg2
+from psycopg2 import errors
+from datetime import datetime
+
+
+class FDataBase:
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.conn = None
+
+    def connect(self):
+        if self.conn is None:
+            self.conn = psycopg2.connect(self.dbname)
+
+    def closed(self):
+        self.conn.close()
+        self.conn = None
+
+    def add_url(self, named: str):
+        """  Add id, url in db.url  """
+        now = datetime.now()
+        try:
+            self.connect()
+            with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
+                curs.execute("""INSERT INTO urls (name, created_at)
+                 VALUES (%s, %s) RETURNING id;""", (named, now))
+                self.conn.commit()
+                return curs.fetchone()["id"]
+        except errors.lookup(UNIQUE_VIOLATION):
+            return 'nouniq'
+
+    def add_urls(self, url_id: id, status_code: id, h1: str, title: str,
+                 description: str):
+        """ Add seo in db.url_checks """
+        now = datetime.now()
+        try:
+            self.connect()
+            with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
+                curs.execute("""INSERT INTO url_checks (url_id, status_code, h1,
+                 title, description, created_at) VALUES (%s, %s, %s, %s, %s,
+                  %s); """, (url_id, status_code, h1, title, description, now))
+                self.conn.commit()
+        finally:
+            self.closed()
+
+    def get_name(self, name: str) -> dict:
+        """ Check and returns name from db.urls """
+        self.connect()
+        try:
+            with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
+                name_ = [name, ]
+                curs.execute("SELECT * FROM urls WHERE name = ANY(%s);",
+                             (name_,))
+                return curs.fetchone()
+        finally:
+            self.closed()
+
+    def get_id(self, id):
+        """ Returns name url and date from db.urls """
+        self.connect()
+        try:
+            with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
+                id_ = [id, ]
+                curs.execute("SELECT * FROM urls WHERE id = ANY(%s);", (id_,))
+                return curs.fetchone()
+        finally:
+            self.closed()
+
+    def get_for_urls(self):
+        """ Returns name id, url, data check, name,
+            status_code from db.url and db.urls """
+        self.connect()
+        try:
+            with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
+                curs.execute("SELECT u.url_id, u.status_code, u.created_at, "
+                             "urls.name from "
+                             "(SELECT url_id, max(created_at) as created_at "
+                             "FROM url_checks GROUP BY url_checks.url_id) as t"
+                             " join url_checks as u on u.url_id = t.url_id "
+                             "and u.created_at = "
+                             "t.created_at "
+                             "join urls on u.url_id = urls.id "
+                             "order by u.url_id DESC", )
+                return curs.fetchall()
+        finally:
+            self.closed()
+
+    def get_for_url_checks(self, url_id):
+        """ Returns seo in db.url_checks """
+        self.connect()
+        try:
+            with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
+                id_ = [url_id, ]
+                curs.execute("SELECT * FROM url_checks WHERE url_id = ANY(%s) "
+                             "ORDER BY created_at DESC;", (id_,))
+                return curs.fetchall()
+        finally:
+            self.closed()
